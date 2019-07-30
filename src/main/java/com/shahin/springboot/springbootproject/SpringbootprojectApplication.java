6907d6730742dc3a3066efd5603d2989baa043ad
reshidshahin <reshidshahin@gmail.com>
2019-07-31 00:57:36 +0400

    Texnologiyalar: Spring Boot, MVC, JPA

---
+++ src/main/java/com/shahin/springboot/springbootproject/SpringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.shahin.springboot.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootprojectApplication.class, args);
+    }
+
+}
